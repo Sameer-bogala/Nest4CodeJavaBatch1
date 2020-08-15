7eabd2eed8e163f6bd7f32a7cf8bc1d1842b3d1d
Sameer-bogala <68263637+Sameer-bogala@users.noreply.github.com>
2020-08-15 21:29:42 +0530

    Add files via upload

---
+++ day1/day1.java
@@ -0,0 +1,18 @@
+
+public class day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("helloworld");
+	       float prod1=70;
+	       float prod2=20;
+	       float prod3=10;
+	       float sumprod=prod1+prod2+prod3;
+	       float taxper=18;
+	       float taxAmount=(taxper/100)*sumprod;
+	       float finalAmount=sumprod+taxAmount;
+	       System.out.println("thefinal amountis"+finalAmount);
+
+	}
+
+}
