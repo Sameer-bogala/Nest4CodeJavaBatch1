322022b415f08c73cad502ba4c44a13221917bd1
Sameer-bogala <68263637+Sameer-bogala@users.noreply.github.com>
2020-08-20 13:20:06 +0530

    Add files via upload

---
+++ day4/day41.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+   public class day41 {
+
+
+
+
+		public static void main(String[] args) {
+			Scanner Scan=new Scanner(System.in);
+			// TODO Auto-generated method stub
+			System.out.println("enter ur number ex=2468:");
+	          int num=Scan.nextInt();
+	          System.out.println("your input number:"+num);
+	          String str=String.valueOf(num);
+	          
+	          int count=0;
+	          if(num>0) {
+	        	     for(int i=0;i<str.length();i++) {
+        		         if(str.charAt(i)%2==1) {
+	        		  
+       			          count+=1;
+       			   
+       		           }
+       		 
+       		 
+       	            } ;
+       	         System.out.println("the required output is :");
+                  System.out.println("the odd value digits="+count);
+                  count=0;
+	        	     for(int i=0;i<str.length();i++) {
+	        		  //System.out.println(str.charAt(i)%2==0);
+	        		    if(str.charAt(i)%2==0) {
+	        			   count+=1;
+	        			
+	        		       }
+	        	  };
+	            
+	            System.out.println("the even value digits="+count);
+	            count=0;
+	        	   
+	          }
+	         else {
+	        	 System.out.println("enter a positive integer:"); 
+	          }
+		}
+
+	}
+
